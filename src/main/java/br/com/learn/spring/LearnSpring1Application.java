5b10226f9c832b7edc8257562766909f33f1bd21
Fidelis Felipe <atosfiel@gmail.com>
2021-06-08 09:07:47 -0300

    init spring started

---
+++ src/main/java/br/com/learn/spring/LearnSpring1Application.java
@@ -0,0 +1,13 @@
+package br.com.learn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpring1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/learn/spring/LearnSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.learn.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
